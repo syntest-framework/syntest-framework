7f0d20cf94cbcf98e2c9a0e289491235b10fa067
apanichella <a.panichella@tudelft.nl>
2020-11-14 13:25:51 +0100

    Add test case fo fast non-dominated sorting

---
+++ test/search/operator/FasNonDomSorting.test.ts
@@ -0,0 +1,30 @@
+import {Objective, tournamentSelection} from "../../../lib";
+import {DummyIndividual} from "../../DummyIndividual.test";
+import {fastNonDomSorting} from "../../../lib/search/operator/FastNonDomSorting";
+
+/**
+ * @author Annibale Panichella
+ */
+describe('Fast non-dominated sorting', function () {
+    test('Sort three solutions', () => {
+        let objective1: Objective = {line: 1, locationIdx: 1};
+        let objective2: Objective = {line: 1, locationIdx: 2};
+
+        let ind1 = new DummyIndividual();
+        ind1.setDummyEvaluation([objective1, objective2], [0, 1])
+
+        let ind2 = new DummyIndividual();
+        ind2.setDummyEvaluation([objective1, objective2], [3, 3])
+
+        let ind3 = new DummyIndividual();
+        ind3.setDummyEvaluation([objective1, objective2], [2, 0])
+
+        const F = fastNonDomSorting([ind1, ind2, ind3])
+        expect(F[0].length).toEqual(2)
+        expect(F[0]).toContain(ind1)
+        expect(F[0]).toContain(ind3)
+        expect(F[1].length).toEqual(1)
+        expect(F[1]).toContain(ind2)
+    })
+
+})
