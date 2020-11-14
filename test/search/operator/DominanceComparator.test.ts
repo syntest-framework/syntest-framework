f65e60ad86e6df9f4150f7db60d860af4b5a0712
apanichella <a.panichella@tudelft.nl>
2020-11-14 13:04:34 +0100

    Improving tests for dominance comparator

--- test/search/operator/DominanceComparator.test.ts
+++ test/search/operator/DominanceComparator.test.ts
@@ -5,77 +5,81 @@ import {DummyIndividual} from "../../DummyIndividual.test";
 /**
  * @author Annibale Panichella
  */
-test('Dominance comparator, fist dominates', () => {
-    let objective1 : Objective = { line: 1, locationIdx: 1 };
-    let objective2 : Objective = { line: 1, locationIdx: 2 };
+describe('Dominance comparator', function () {
 
-    let ind1 = new DummyIndividual();
-    ind1.setDummyEvaluation([objective1, objective2], [0, 1])
+    test('Fist individual dominates', () => {
+        let objective1: Objective = {line: 1, locationIdx: 1};
+        let objective2: Objective = {line: 1, locationIdx: 2};
 
-    let ind2 = new DummyIndividual();
-    ind2.setDummyEvaluation([objective1, objective2], [1, 1])
+        let ind1 = new DummyIndividual();
+        ind1.setDummyEvaluation([objective1, objective2], [0, 1])
 
-    let set = new Set<Objective>();
-    set.add(objective1)
-    set.add(objective2)
-    let value = DominanceComparator.compare(ind1, ind2, set)
+        let ind2 = new DummyIndividual();
+        ind2.setDummyEvaluation([objective1, objective2], [1, 1])
 
-    expect(value == -1)
-})
+        let set = new Set<Objective>();
+        set.add(objective1)
+        set.add(objective2)
+        let value = DominanceComparator.compare(ind1, ind2, set)
 
-test('Dominance comparator, second dominates', () => {
-    let objective1 : Objective = { line: 1, locationIdx: 1 };
-    let objective2 : Objective = { line: 1, locationIdx: 2 };
+        expect(value).toEqual(-1)
+    })
 
-    let ind1 = new DummyIndividual();
-    ind1.setDummyEvaluation([objective1, objective2], [1, 1])
+    test('Second individual dominates', () => {
+        let objective1: Objective = {line: 1, locationIdx: 1};
+        let objective2: Objective = {line: 1, locationIdx: 2};
 
-    let ind2 = new DummyIndividual();
-    ind2.setDummyEvaluation([objective1, objective2], [1, 0])
+        let ind1 = new DummyIndividual();
+        ind1.setDummyEvaluation([objective1, objective2], [1, 1])
 
-    let set = new Set<Objective>();
-    set.add(objective1)
-    set.add(objective2)
-    let value = DominanceComparator.compare(ind1, ind2, set)
+        let ind2 = new DummyIndividual();
+        ind2.setDummyEvaluation([objective1, objective2], [1, 0])
 
-    expect(value == 1)
-})
+        let set = new Set<Objective>();
+        set.add(objective1)
+        set.add(objective2)
+        let value = DominanceComparator.compare(ind1, ind2, set)
 
-test('Dominance comparator, none dominates', () => {
-    let objective1 : Objective = { line: 1, locationIdx: 1 };
-    let objective2 : Objective = { line: 1, locationIdx: 2 };
+        expect(value).toEqual(1)
+    })
 
-    let ind1 = new DummyIndividual();
-    ind1.setDummyEvaluation([objective1, objective2], [1, 1])
+    test('None dominates with two objectives', () => {
+        let objective1: Objective = {line: 1, locationIdx: 1};
+        let objective2: Objective = {line: 1, locationIdx: 2};
 
-    let ind2 = new DummyIndividual();
-    ind2.setDummyEvaluation([objective1, objective2], [1, 1])
+        let ind1 = new DummyIndividual();
+        ind1.setDummyEvaluation([objective1, objective2], [1, 1])
 
-    let set = new Set<Objective>();
-    set.add(objective1)
-    set.add(objective2)
+        let ind2 = new DummyIndividual();
+        ind2.setDummyEvaluation([objective1, objective2], [1, 1])
 
-    let value = DominanceComparator.compare(ind1, ind2, set)
+        let set = new Set<Objective>();
+        set.add(objective1)
+        set.add(objective2)
 
-    expect(value == 0)
-})
+        let value = DominanceComparator.compare(ind1, ind2, set)
 
-test('Dominance comparator, none dominates three objective', () => {
-    let objective1 : Objective = { line: 1, locationIdx: 1 };
-    let objective2 : Objective = { line: 1, locationIdx: 2 };
-    let objective3 : Objective = { line: 1, locationIdx: 3 };
+        expect(value).toEqual(0)
+    })
 
-    let ind1 = new DummyIndividual();
-    ind1.setDummyEvaluation([objective1, objective2, objective3], [1, 0, 1])
+    test('None dominates with three objective', () => {
+        let objective1: Objective = {line: 1, locationIdx: 1};
+        let objective2: Objective = {line: 1, locationIdx: 2};
+        let objective3: Objective = {line: 1, locationIdx: 3};
 
-    let ind2 = new DummyIndividual();
-    ind2.setDummyEvaluation([objective1, objective2, objective3], [0, 1, 1])
+        let ind1 = new DummyIndividual();
+        ind1.setDummyEvaluation([objective1, objective2, objective3], [1, 0, 1])
 
-    let set = new Set<Objective>();
-    set.add(objective1)
-    set.add(objective2)
-    set.add(objective3)
+        let ind2 = new DummyIndividual();
+        ind2.setDummyEvaluation([objective1, objective2, objective3], [0, 1, 1])
 
-    let value = DominanceComparator.compare(ind1, ind2, set)
-    expect(value == 0)
+        let set = new Set<Objective>();
+        set.add(objective1)
+        set.add(objective2)
+        set.add(objective3)
+
+        let value = DominanceComparator.compare(ind1, ind2, set)
+        expect(value).toEqual(0)
+    })
+
 })
