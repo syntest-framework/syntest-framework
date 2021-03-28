f2a034a25133a9e05cfa7d5a45714893fbea97ae
Mitchell Olsthoorn <mitchell.olsthoorn@outlook.com>
2021-03-28 19:06:00 +0200

    Add comments to the EncodingSampler

--- src/search/EncodingSampler.ts
+++ src/search/EncodingSampler.ts
@@ -1,5 +1,11 @@
 import { Encoding } from "./Encoding";
 
+/**
+ * Sampler for encodings.
+ */
 export interface EncodingSampler<T extends Encoding<T>> {
+  /**
+   * Sample an encoding.
+   */
   sample(): T;
 }
