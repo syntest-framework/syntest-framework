bdb5061c531255b3a87fc9da39351788206e54c2
Mitchell Olsthoorn <mitchell.olsthoorn@outlook.com>
2021-03-28 23:19:00 +0200

    Comment COMIX and MultiGA

--- src/search/metaheuristics/COMIX.ts
+++ src/search/metaheuristics/COMIX.ts
@@ -1,24 +1,24 @@
-import { GeneticAlgorithm } from "./GeneticAlgorithm";
-import { TestCase } from "../..";
-import { MultiGA } from "./MultiGA";
-
-export class COMIX<T extends GeneticAlgorithm> extends MultiGA<T> {
-  async multiGeneration(): Promise<void> {
-    // pick champions
-    const champions: TestCase[] = [];
-    for (const algorithm of this.subAlgorithms) {
-      champions.push(algorithm.population[0]);
-    }
-
-    // migrate champions
-    for (const algorithm of this.subAlgorithms) {
-      algorithm.population.push(...champions);
-      // TODO might be able to parallelized
-      algorithm.population = await algorithm.generation(algorithm.population);
-    }
-  }
-
-  getCurrentCoverage(): number {
-    return 0; // TODO
-  }
-}
+// import { GeneticAlgorithm } from "./GeneticAlgorithm";
+// import { TestCase } from "../..";
+// import { MultiGA } from "./MultiGA";
+//
+// export class COMIX<T extends GeneticAlgorithm> extends MultiGA<T> {
+//   async multiGeneration(): Promise<void> {
+//     // pick champions
+//     const champions: TestCase[] = [];
+//     for (const algorithm of this.subAlgorithms) {
+//       champions.push(algorithm.population[0]);
+//     }
+//
+//     // migrate champions
+//     for (const algorithm of this.subAlgorithms) {
+//       algorithm.population.push(...champions);
+//       // TODO might be able to parallelized
+//       algorithm.population = await algorithm.generation(algorithm.population);
+//     }
+//   }
+//
+//   getCurrentCoverage(): number {
+//     return 0; // TODO
+//   }
+// }
