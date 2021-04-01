ddbcff84543e94288c40348b77b91b7acf06e9ea
apanichella <a.panichella@tudelft.nl>
2021-04-01 16:11:55 +0200

    Update factory with new changes
    Co-authored-by: Mitchell Olsthoorn <mitchell.olsthoorn@outlook.com>

--- src/search/factories/AlgorithmFactory.ts
+++ src/search/factories/AlgorithmFactory.ts
@@ -1,70 +1,37 @@
 import { getProperty } from "../../config";
 import {
-  COMIX,
-  Fitness,
-  GeneticAlgorithm,
+  ObjectiveFunction,
+  EvolutionaryAlgorithm,
   MOSA,
-  MultiGA,
-  NSGA2,
+  NSGAII,
   TestCaseSampler,
-  SimpleGA,
-  Target,
-} from "../..";
+  SearchSubject,
+  TestCase, TestCaseRunner
+} from "../../";
+import {SearchAlgorithm} from "../metaheuristics/SearchAlgorithm";
+import {RandomSearch} from "../metaheuristics/RandomSearch";
+import {DynaMOSA} from "../metaheuristics/evolutionary/mosa/DynaMOSA";
 
 /**
  *
  * @author Dimitri Stallenberg
  */
 export function createAlgorithmFromConfig(
-  target: Target | Target[],
-  fitnessObject: Fitness,
-  sampler: TestCaseSampler
-) {
-  const singleAlgoritms: {
-    [key: string]: { new (...args: any[]): GeneticAlgorithm };
-  } = {
-    SimpleGA: SimpleGA,
-    NSGA2: NSGA2,
-    MOSA: MOSA,
-  };
-
-  const multiAlgorithms: {
-    [key: string]: { new (...args: any[]): MultiGA<any> };
-  } = {
-    COMIX: COMIX,
-  };
+  sampler: TestCaseSampler,
+  runner: TestCaseRunner
+): SearchAlgorithm<TestCase>{
 
   const algorithm = getProperty("algorithm");
 
-  if (algorithm in singleAlgoritms) {
-    if (target instanceof Array) {
-      throw new Error(
-        `Cannot use multiple target for the single target algorithms.`
-      );
-    }
-
-    return new singleAlgoritms[algorithm](target, fitnessObject, sampler);
-  } else if (algorithm in multiAlgorithms) {
-    if (target instanceof Target) {
-      throw new Error(
-        `Cannot use single target for the multi target algorithms.`
-      );
-    }
-
-    const subAlgorithm = getProperty("subAlgorithm");
-    if (!(subAlgorithm in singleAlgoritms)) {
-      throw new Error(
-        `${subAlgorithm} is not a valid sub-algorithm. (Multi-algorithms cannot use other multi-algorithms as sub-algorithms)`
-      );
-    }
-
-    return new multiAlgorithms[algorithm](
-      target,
-      fitnessObject,
-      sampler,
-      singleAlgoritms[subAlgorithm]
-    );
-  } else {
-    throw new Error(`${algorithm} is not a valid algorithm.`);
+  switch(algorithm){
+    case "Random":
+      return new RandomSearch(sampler, runner);
+    case "NSGAII":
+      return new NSGAII(sampler,runner);
+    case "MOSA":
+      return new MOSA(sampler, runner);
+    case "DynaMOSA":
+      return new DynaMOSA(sampler, runner);
   }
+
 }
