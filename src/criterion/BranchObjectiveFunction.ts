70bcf0f17d6a8400a3ab6ff8e499ebe371a60721
apanichella <a.panichella@tudelft.nl>
2021-04-08 00:47:00 +0200

    Simplify the approach level computation

--- src/criterion/BranchObjectiveFunction.ts
+++ src/criterion/BranchObjectiveFunction.ts
@@ -4,6 +4,8 @@ import { Node } from "../graph/Node";
 import { getLogger } from "../util/logger";
 import { SearchSubject } from "../search/SearchSubject";
 import { BranchDistance } from "../search/objective/BranchDistance";
+import {FunctionObjectiveFunction} from "./FunctionObjectiveFunction";
+import {ProbeObjectiveFunction} from "./ProbeObjectiveFunction";
 
 /**
  * Objective function for the branch criterion.
@@ -48,100 +50,68 @@ export class BranchObjectiveFunction<T extends Encoding>
    */
   calculateDistance(encoding: T): number {
     const executionResult = encoding.getExecutionResult();
-    // calculate coverage for the branches
-    const hitNodes = [];
+    // let's check if the line is covered
+    if (executionResult.coversLine(this._line)){
+      const branchTrace = executionResult.getTraces().find((trace) =>
+          trace.type === "branch" &&
+          trace.line === this._line &&
+          trace.locationIdx === this._locationIdx
+      )
 
-    for (const trace of executionResult.getTraces()) {
-      // Check if it is a branch node and has been hit
-      if (trace.type !== "branch" || trace.hits === 0) {
-        continue;
+      if (branchTrace.hits > 0){
+        return 0;
+      } else {
+        const oppositeBranch = executionResult.getTraces().find((trace) =>
+            trace.type === "branch" &&
+            trace.id === branchTrace.id &&   // same branch id
+            trace.locationIdx !== this._locationIdx  // different location (0 = false, 1 = true)
+        )
+
+        return BranchDistance.branchDistanceNumeric(
+            oppositeBranch.opcode,
+            oppositeBranch.left,
+            oppositeBranch.right,
+            this._type);
       }
-
-      // Check if the branch in question is currently an objective
-      const objective = this._subject
-        .getObjectives()
-        .filter((objective) => objective instanceof BranchObjectiveFunction)
-        .find((objective) => {
-          const branchObjective = <BranchObjectiveFunction<T>>objective;
-          return (
-            branchObjective._locationIdx === trace.locationIdx &&
-            branchObjective._line === trace.line
-          );
-        });
-
-      if (!objective) {
-        continue;
-      }
-
-      // find the corresponding branch node inside the cfg
-      const branchNode = this._subject.cfg.nodes.find((n: Node) => {
-        return n.locationIdx === trace.locationIdx && n.line === trace.line;
-      });
-
-      if (!branchNode) {
-        getLogger().error("Branch node not found!");
-        process.exit(1);
-      }
-
-      // record hits
-      hitNodes.push({
-        node: branchNode,
-        point: trace,
-      });
     }
 
-    const nodes = this._subject.cfg.nodes.filter(
-      (n: any) => n.functionDefinition || n.branchId
-    );
-
-    // find the node in the CFG object that corresponds to the objective
-    const node = nodes.find((n) => {
-      return this._locationIdx === n.locationIdx && this._line === n.line;
+    // find the corresponding branch node inside the cfg
+    const branchNode = this._subject.cfg.nodes.find((n: Node) => {
+      return n.locationIdx === this._locationIdx && n.line === this._line;
     });
 
-    // No node found so the objective is uncoverable
-    if (!node) {
-      return Number.MAX_VALUE;
-    }
-
-    // find if the branch was covered
-    const hitNode = hitNodes.find((h: any) => h.node === node);
-
-    // if it is covered the distance is 0
-    if (hitNode) {
-      return 0;
-    }
 
     // find the closest covered branch to the objective branch
     let closestHitNode = null;
-    let smallestDistance = Number.MAX_VALUE;
-    for (const n of hitNodes) {
-      const pathDistance = this._subject.getPath(node.id, n.node.id);
-      if (smallestDistance > pathDistance) {
-        smallestDistance = pathDistance;
-        closestHitNode = n;
+    let approachLevel = Number.MAX_VALUE;
+    for (const n of this._subject.cfg.nodes) {
+      const traces = executionResult.getTraces().filter((trace) =>
+          trace.line === n.line &&
+          (trace.type === "branch" || trace.type === "probePre" || trace.type === "probePost") &&
+          trace.hits > 0
+      )
+      for (const trace of traces){
+        const pathDistance = this._subject.getPath(n.id, branchNode.id);
+        if (approachLevel > pathDistance) {
+          approachLevel = pathDistance;
+          closestHitNode = trace;
+        }
       }
     }
 
-    if (!closestHitNode) {
+    // if closer node (branch or probe) is not found, we return the distance to the root branch
+    if (!closestHitNode){
       return Number.MAX_VALUE;
     }
 
     // calculate the branch distance between: covering the branch needed to get a closer approach distance and the currently covered branch
     // always between 0 and 1
     const branchDistance = BranchDistance.branchDistanceNumeric(
-      closestHitNode.point.opcode,
-      closestHitNode.point.left,
-      closestHitNode.point.right,
-      this._type // we look at whether we are optimizing the false or the true branch
+      closestHitNode.opcode,
+      closestHitNode.left,
+      closestHitNode.right,
+      closestHitNode.locationIdx == 1 ? false : true // we have to revert/negate the condition for the closest covered node
     );
-
-    let approachLevel = smallestDistance;
-    const lineCoverage = executionResult
-      .getTraces()
-      .filter((n: any) => n.line == node.line && n.type == "line");
-
-    if (lineCoverage.length > 0 && lineCoverage[0].hits > 0) approachLevel = 0;
 
     // add the distances
     const distance = approachLevel + branchDistance;
