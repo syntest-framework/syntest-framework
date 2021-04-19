cdeefd6c1685a47ca92d8bd7e369f82d5787f5d1
apanichella <a.panichella@tudelft.nl>
2021-04-19 18:02:03 +0200

    Handle the termination of MOSA/DynaMOSA when 100% coverage is reached

--- src/search/metaheuristics/evolutionary/mosa/MOSA.ts
+++ src/search/metaheuristics/evolutionary/mosa/MOSA.ts
@@ -27,6 +27,18 @@ export class MOSA extends EvolutionaryAlgorithm {
   }
 
   protected _environmentalSelection(size: number): void {
+    if (
+        this._objectiveManager.getCurrentObjectives().size == 0 &&
+        this._objectiveManager.getUncoveredObjectives().size != 0
+    )
+      throw Error("This should never happen. There is a likely bug in the objective manager");
+
+    if (
+        this._objectiveManager.getCurrentObjectives().size == 0 &&
+        this._objectiveManager.getUncoveredObjectives().size == 0
+    )
+      return; // the search should end
+
     // non-dominated sorting
     getLogger().debug(
       "Number of objectives = " +
