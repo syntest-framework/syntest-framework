4348d16ee3ec804000be62d0bd29022cc5baa542
Mitchell Olsthoorn <mitchell.olsthoorn@outlook.com>
2021-04-20 01:05:20 +0200

    Remove accidental commited code

--- src/search/SearchListener.ts
+++
@@ -1,4 +0,0 @@
-import { Encoding } from "./Encoding";
-import { SearchAlgorithm } from "./metaheuristics/SearchAlgorithm";
-
-export abstract class SearchListener<T extends Encoding> {}
--- src/search/objective/secondary/SecondaryObjectiveComparator.ts
+++
@@ -1,5 +0,0 @@
-import { Encoding } from "../../Encoding";
-
-export interface SecondaryObjectiveComparator<T extends Encoding> {
-  compare(encoding1: Encoding, encoding2: Encoding): number;
-}
