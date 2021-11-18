a92fc2b6179e3664ff145f70e607fdeb89714838
Dimitri Stallenberg <tweet.dimitri@gmail.com>
2021-11-18 11:49:06 +0100

    Implement basic launcher

--- src/Launcher.ts
+++ src/Launcher.ts
@@ -16,6 +16,94 @@
  * limitations under the License.
  */
 
+import {
+  Archive,
+  guessCWD,
+  loadConfig,
+  loadTargets,
+  processConfig,
+  setupLogger,
+  setupOptions,
+} from "@syntest/framework";
+import { JavaScriptTestCase } from "./testcase/JavaScriptTestCase";
+import { TargetPool } from "./cache/TargetPool";
+
 export class Launcher {
-  public async run() {}
+  private readonly _program = "syntest-javascript";
+
+  public async run() {
+    await guessCWD(null)
+    const [included, excluded] = await this.setup()
+    const [archive, imports, dependencies] = await this.search(
+      included,
+      excluded
+    );
+    await this.finalize(archive, imports, dependencies);
+
+    await exit()
+  }
+
+  private async setup(): Promise<[Map<string, string[]>, Map<string, string[]>]> {
+    // Filesystem & Compiler Re-configuration
+    const additionalOptions = {}; // TODO
+    setupOptions(this._program, additionalOptions);
+
+    const index = process.argv.indexOf(process.argv.find((a) => a.includes(this._program)))
+    const args = process.argv.slice(index + 1);
+
+    const config = loadConfig(args);
+    processConfig(config, args);
+    setupLogger();
+
+    // TODO ui setup
+
+    const [included, excluded] = await loadTargets();
+    if (!included.size) {
+      // TODO ui error
+      process.exit(1);
+    }
+
+    // TODO ui info messages
+
+    return [included, excluded]
+  }
+
+  private async search(
+    included: Map<string, string[]>,
+    excluded: Map<string, string[]>
+  ): Promise<
+    [Archive<JavaScriptTestCase>, Map<string, string>, Map<string, string[]>]
+    > {
+    const targetPool = new TargetPool()
+
+
+    // TODO setup temp folders
+
+    // TODO instrument targets
+
+    // TODO save instrumented files
+
+    const finalArchive = new Archive<JavaScriptTestCase>();
+    let finalImports: Map<string, string> = new Map();
+    let finalDependencies: Map<string, string[]> = new Map();
+
+    // TODO search targets
+
+    return [finalArchive, finalImports, finalDependencies]
+  }
+
+  private async finalize(
+    archive: Archive<JavaScriptTestCase>,
+    imports: Map<string, string>,
+    dependencies: Map<string, string[]>
+  ): Promise<void> {
+
+  }
+
+  async exit(): Promise<void> {
+    // Finish
+    // TODO delete temp folders
+
+    process.exit(0);
+  }
 }
