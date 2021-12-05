0454687b61bab615b86fbb9bcdc2b4ab256608e9
Dimitri <tweet.dimitri@gmail.com>
2021-12-05 16:57:18 +0100

    Create Primitive Types

---
+++ src/testcase/statements/primitive/NumericStatement.ts
@@ -0,0 +1,107 @@
+/*
+ * Copyright 2020-2021 Delft University of Technology and SynTest contributors
+ *
+ * This file is part of SynTest Solidity.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import {
+  PrimitiveStatement,
+  TestCaseSampler,
+  prng,
+  Properties,
+  Parameter,
+} from "@syntest/framework";
+
+/**
+ * Generic number class
+ *
+ * Uses BigNumber to allow for numbers larger than allowed by javascript.
+ *
+ * Documentation on BigNumber:
+ * https://www.npmjs.com/package/bignumber.js
+ *
+ * @author Dimitri Stallenberg
+ */
+export class NumericStatement extends PrimitiveStatement<number> {
+  constructor(
+    type: Parameter,
+    uniqueId: string,
+    value: number
+  ) {
+    super(type, uniqueId, value);
+  }
+
+  mutate(sampler: TestCaseSampler, depth: number): NumericStatement {
+    if (prng.nextBoolean(Properties.delta_mutation_probability)) {
+      return this.deltaMutation();
+    }
+
+    return NumericStatement.getRandom(
+      this.type
+    );
+  }
+
+  deltaMutation() {
+    // small mutation
+    let change = prng.nextGaussian(0, 20);
+
+    if (this.type.type.includes("int")) {
+      change = Math.round(change);
+      if (change == 0) change = prng.nextBoolean() ? -1 : 1;
+    }
+
+    let newValue = this.value + change;
+
+    // If illegal values are not allowed we make sure the value does not exceed the specified bounds
+    if (!Properties.explore_illegal_values) {
+      const max = Number.MAX_SAFE_INTEGER;
+      const min = Number.MIN_SAFE_INTEGER;
+
+      if (newValue > max) {
+        newValue = max;
+      } else if (newValue < min) {
+        newValue = min;
+      }
+    }
+
+    return new NumericStatement(
+      this.type,
+      this.id,
+      newValue
+    );
+  }
+
+  copy() {
+    return new NumericStatement(
+      this.type,
+      prng.uniqueId(),
+      this.value
+    );
+  }
+
+  static getRandom(
+    type: Parameter = { type: "number", name: "noname" }
+  ) {
+    // by default we create small numbers (do we need very large numbers?)
+    const max = Number.MAX_SAFE_INTEGER
+    const min = Number.MIN_SAFE_INTEGER
+
+    return new NumericStatement(
+      type,
+      prng.uniqueId(),
+      prng.nextDouble(min, max),
+    );
+  }
+}
---
+++ src/testcase/statements/primitive/StringStatement.ts
@@ -0,0 +1,213 @@
+/*
+ * Copyright 2020-2021 Delft University of Technology and SynTest contributors
+ *
+ * This file is part of SynTest Solidity.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import {
+  PrimitiveStatement,
+  TestCaseSampler,
+  prng,
+  Properties,
+  Parameter,
+} from "@syntest/framework";
+
+/**
+ * @author Dimitri Stallenberg
+ */
+export class StringStatement extends PrimitiveStatement<string> {
+  private readonly alphabet: string;
+  private readonly maxlength: number;
+
+  constructor(
+    type: Parameter,
+    uniqueId: string,
+    value: string,
+    alphabet: string,
+    maxlength: number
+  ) {
+    super(type, uniqueId, value);
+    this.alphabet = alphabet;
+    this.maxlength = maxlength;
+  }
+
+  mutate(sampler: TestCaseSampler, depth: number): StringStatement {
+    if (prng.nextBoolean(Properties.resample_gene_probability)) {
+      return StringStatement.getRandom();
+    }
+
+    if (this.value.length > 0 && this.value.length < this.maxlength) {
+      const value = prng.nextInt(0, 3);
+
+      if (value === 0) {
+        return this.addMutation();
+      } else if (value === 1) {
+        return this.removeMutation();
+      } else if (value === 2) {
+        return this.replaceMutation();
+      } else {
+        return this.deltaMutation();
+      }
+    } else if (this.value.length > 0) {
+      const value = prng.nextInt(0, 2);
+
+      if (value === 0) {
+        return this.removeMutation();
+      } else if (value === 1) {
+        return this.replaceMutation();
+      } else {
+        return this.deltaMutation();
+      }
+    } else {
+      return this.addMutation();
+    }
+  }
+
+  addMutation(): StringStatement {
+    const position = prng.nextInt(0, this.value.length - 1);
+    const addedChar = prng.pickOne(this.alphabet);
+
+    let newValue = "";
+
+    for (let i = 0; i < this.value.length; i++) {
+      if (i < position || i > position) {
+        newValue += this.value[i];
+      } else {
+        newValue += addedChar;
+        newValue += this.value[i];
+      }
+    }
+
+    return new StringStatement(
+      this.type,
+      this.id,
+      newValue,
+      this.alphabet,
+      this.maxlength
+    );
+  }
+
+  removeMutation(): StringStatement {
+    const position = prng.nextInt(0, this.value.length - 1);
+
+    let newValue = "";
+
+    for (let i = 0; i < this.value.length; i++) {
+      if (i === position) {
+        continue;
+      }
+      newValue += this.value[i];
+    }
+
+    return new StringStatement(
+      this.type,
+      this.id,
+      newValue,
+      this.alphabet,
+      this.maxlength
+    );
+  }
+
+  replaceMutation(): StringStatement {
+    const position = prng.nextInt(0, this.value.length - 1);
+    const newChar = prng.pickOne(this.alphabet);
+
+    let newValue = "";
+
+    for (let i = 0; i < this.value.length; i++) {
+      if (i < position || i > position) {
+        newValue += this.value[i];
+      } else {
+        newValue += newChar;
+      }
+    }
+
+    return new StringStatement(
+      this.type,
+      this.id,
+      newValue,
+      this.alphabet,
+      this.maxlength
+    );
+  }
+
+  deltaMutation(): StringStatement {
+    const position = prng.nextInt(0, this.value.length - 1);
+    const oldChar = this.value[position];
+    const indexOldChar = this.alphabet.indexOf(oldChar);
+    const delta = prng.pickOne([-2, -1, 1, -2]);
+    const newChar =
+      this.alphabet[(indexOldChar + delta) % this.alphabet.length];
+
+    let newValue = "";
+
+    for (let i = 0; i < this.value.length; i++) {
+      if (i < position || i > position) {
+        newValue += this.value[i];
+      } else {
+        newValue += newChar;
+      }
+    }
+
+    return new StringStatement(
+      this.type,
+      this.id,
+      newValue,
+      this.alphabet,
+      this.maxlength
+    );
+  }
+
+  copy(): StringStatement {
+    return new StringStatement(
+      this.type,
+      this.id,
+      this.value,
+      this.alphabet,
+      this.maxlength
+    );
+  }
+
+  static getRandom(
+    type: Parameter = { type: "string", name: "noname" },
+    alphabet = Properties.string_alphabet,
+    maxlength = Properties.string_maxlength
+  ): StringStatement {
+    const valueLength = prng.nextInt(0, maxlength - 1);
+    let value = "";
+
+    for (let i = 0; i < valueLength; i++) {
+      value += prng.pickOne(alphabet);
+    }
+
+    return new StringStatement(
+      type,
+      prng.uniqueId(),
+      value,
+      alphabet,
+      maxlength
+    );
+  }
+
+  static createWithValue(type: Parameter, value: string): StringStatement {
+    return new StringStatement(
+      type,
+      prng.uniqueId(),
+      value,
+      Properties.string_alphabet,
+      Properties.string_maxlength
+    );
+  }
+}
