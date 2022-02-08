34789e9723e2c3d89b874aaff514e3012d3322f0
Dimitri Stallenberg <tweet.dimitri@gmail.com>
2022-02-08 13:49:39 +0100

    Fix merge conflicts

--- src/index.ts
+++ src/index.ts
@@ -110,10 +110,6 @@ export * from "./util/fileSystem";
 export * from "./properties";
 
 // Targetting
-<<<<<<< HEAD
-export * from "./analysis/static/TargetFile";
-=======
 export * from "./analysis/static/Target";
->>>>>>> main
 export * from "./analysis/static/TargetPool";
 export * from "./analysis/static/targetUtil";
