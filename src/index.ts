fb7234c7e6edb7834664f7f048f41d2f8c070dbd
Dimitri Stallenberg <tweet.dimitri@gmail.com>
2023-01-12 14:47:48 +0100

    Create and export Eventmanager

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
 /*
- * Copyright 2020-2021 Delft University of Technology and SynTest contributors
+ * Copyright 2020-2023 Delft University of Technology and SynTest contributors
  *
  * This file is part of SynTest Framework.
  *
@@ -104,3 +104,7 @@ export * from "./properties";
 export * from "./analysis/static/Target";
 export * from "./analysis/static/TargetPool";
 export * from "./analysis/static/targetUtil";
+
+// Events
+export * from "./EventManager"
+export * from "@syntest/core-plugin-interface"
