e4b66b70aba93525cfb0ce77065fed54c8d0d061
Dimitri Stallenberg <dimitrist@Dimitris-MacBook-Pro.local>
2023-02-03 10:00:21 +0100

    fix: export removeindex type

--- packages/core/lib/Configuration.ts
+++ packages/core/lib/Configuration.ts
@@ -20,7 +20,7 @@ import Yargs = require("yargs");
 import * as path from "path";
 import shell = require("shelljs");
 
-type RemoveIndex<T> = {
+export type RemoveIndex<T> = {
   [K in keyof T as string extends K
     ? never
     : number extends K
