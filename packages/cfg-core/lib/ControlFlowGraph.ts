7b035d1cbd26c884e17131910fac4b06089a2834
Dimitri Stallenberg <tweet.dimitri@gmail.com>
2023-02-28 15:29:36 +0100

    chore: fix lodash dependency

--- packages/cfg-core/lib/ControlFlowGraph.ts
+++ packages/cfg-core/lib/ControlFlowGraph.ts
@@ -19,7 +19,7 @@
 import { Node, NodeType } from "./nodes/Node";
 import { Edge } from "./Edge";
 import { Pair } from "./util/Pair";
-import { cloneDeep } from "lodash";
+import { cloneDeep } from "lodash.clonedeep";
 
 export class ControlFlowGraph {
   private readonly _nodes: Node[];
