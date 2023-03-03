7a8ae88d63d8bbfcdbd1fa86d3b1ca4dc68e2c61
Mitchell Olsthoorn <mitchell.olsthoorn@outlook.com>
2023-03-03 17:49:14 +0100

    style: prevent duplicate import statements

--- libraries/core/test/mocks/DummyEncoding.mock.ts
+++ libraries/core/test/mocks/DummyEncoding.mock.ts
@@ -15,8 +15,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-import { BranchObjectiveFunction } from "../../lib";
-import { Encoding } from "../../lib";
+import { BranchObjectiveFunction, Encoding } from "../../lib";
 
 export class DummyEncodingMock extends Encoding {
   private static counter = 0;
