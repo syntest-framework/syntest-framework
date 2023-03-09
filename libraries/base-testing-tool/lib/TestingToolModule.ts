ecc4f76d46ff43810c5847cb2171ce70e76f87a1
Dimitri Stallenberg <tweet.dimitri@gmail.com>
2023-03-09 15:52:19 +0100

    fix: make sure plugins are loaded on time

---
+++ libraries/base-testing-tool/lib/TestingToolModule.ts
@@ -0,0 +1,44 @@
+/*
+ * Copyright 2020-2023 Delft University of Technology and SynTest contributors
+ *
+ * This file is part of SynTest Framework - SynTest Core.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+import { Module, Plugin } from "@syntest/module";
+import { SimpleObjectiveManagerPlugin } from "./plugins/objectiveManagers/SimpleObjectiveManagerPlugin";
+import { StructuralObjectiveManagerPlugin } from "./plugins/objectiveManagers/StructuralObjectiveManagerPlugin";
+import { UncoveredObjectiveManagerPlugin } from "./plugins/objectiveManagers/UncoveredObjectiveManagerPlugin";
+import { DefaultOffspringPlugin } from "./plugins/offspringOperators/DefaultOffspringPlugin";
+import { MOSAFamilyPlugin } from "./plugins/searchAlgorithms/MOSAFamilyPlugin";
+import { NSGAIIPlugin } from "./plugins/searchAlgorithms/NSGAIIPlugin";
+import { RandomSearchPlugin } from "./plugins/searchAlgorithms/RandomSearchPlugin";
+import { SignalTerminationTriggerPlugin } from "./plugins/terminationTriggers/SignalTerminationTriggerPlugin";
+
+export abstract class TestingToolModule extends Module {
+  getPlugins(): Plugin[] {
+    return [
+      new SimpleObjectiveManagerPlugin(),
+      new StructuralObjectiveManagerPlugin(),
+      new UncoveredObjectiveManagerPlugin(),
+
+      new DefaultOffspringPlugin(),
+
+      new MOSAFamilyPlugin(),
+      new NSGAIIPlugin(),
+      new RandomSearchPlugin(),
+
+      new SignalTerminationTriggerPlugin(),
+    ];
+  }
+}
