37bc914b9c8fa8e47cc1e7f918c2861a5e52293c
Mitchell Olsthoorn <mitchell.olsthoorn@outlook.com>
2023-06-25 13:48:44 +0200

    feat: add encoding age

--- libraries/search/lib/Encoding.ts
+++ libraries/search/lib/Encoding.ts
@@ -34,6 +34,7 @@ export abstract class Encoding {
   protected _id: string;
   protected _assertions: Map<string, string>;
   protected _metaComments: string[];
+  protected _age: number;
 
   /**
    * Mapping from objective to their distance values for this test case.
@@ -81,6 +82,14 @@ export abstract class Encoding {
 
   get id(): string {
     return this._id;
+  }
+
+  get age(): number {
+    return this._age;
+  }
+
+  set age(value: number) {
+    this._age = value;
   }
 
   get assertions(): Map<string, string> {
