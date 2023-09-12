e419b3ebf3776d5d4c79f472262a5fbf4928d6a7
Mitchell Olsthoorn <mitchell.olsthoorn@outlook.com>
2023-09-12 17:28:59 +0200

    style: update search algorithm directory name (#354)
    The old name 'metaheuristics' only covers a subset of possible algorithms that could be used for the purpose of this module. Renaming the directory to algorithms opens up the possibility of extending these with additional algorithms later.

--- libraries/search/lib/budget/BudgetManager.ts
+++ libraries/search/lib/budget/BudgetManager.ts
@@ -16,8 +16,8 @@
  * limitations under the License.
  */
 
+import { SearchAlgorithm } from "../algorithms/SearchAlgorithm";
 import { Encoding } from "../Encoding";
-import { SearchAlgorithm } from "../metaheuristics/SearchAlgorithm";
 
 import { Budget } from "./Budget";
 import { BudgetListener } from "./BudgetListener";
